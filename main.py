49e5fd9f2ef763d7d28a0618b5b19ebf92690dd2
aaravkothari <aaravkothari155@gmail.com>
2026-06-08 17:21:14 -0400

    circles annotation

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ def main():
                                         read_from_stub=True,
                                         stub_path="stubs/track_stubs.pkl")
 
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
     # Save video
-    save_video(video_frames, "output_videos/output_video.avi")
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 if __name__ == "__main__":
     main()
