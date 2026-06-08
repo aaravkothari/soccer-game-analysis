f1084bcd1a441f88d1aee19e6afb70146d672293
aaravkothari <aaravkothari155@gmail.com>
2026-06-08 16:48:16 -0400

    initial tracking

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from utils import read_video, save_video
+from tracking import Tracker
+
 def main():
-    print("Hello, World!")
+    # Read video
+    video_frames = read_video("input_videos/08fd33_4.mp4")
+
+    # Initialize Tracker
+    tracker = Tracker("models/best.pt")
+
+    tracks = tracker.get_object_tracks(video_frames,
+                                        read_from_stub=True,
+                                        stub_path="stubs/track_stubs.pkl")
+
+    # Save video
+    save_video(video_frames, "output_videos/output_video.avi")
 
 if __name__ == "__main__":
     main()
